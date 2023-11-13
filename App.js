2f9488216767d583a978e8578633692ad81beffa
Mateus Alves <mateusmpa@gmail.com>
2023-11-12 21:32:30 -0300

    Change deprecated 'expo-app-loading' to 'expo-splash-screen'

--- App.js
+++ App.js
@@ -1,24 +1,49 @@
-import { StatusBar, SafeAreaView } from 'react-native';
-import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
-import AppLoading from 'expo-app-loading';
+import React, { useCallback, useEffect, useState } from 'react';
+import { StatusBar, SafeAreaView, View } from 'react-native';
+import { Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
 
 import Cesta from './src/telas/Cesta';
 import mock from './src/mocks/cesta';
 
 export default function App() {
-  const [fonteCarregada] = useFonts({
-    "MontserratRegular": Montserrat_400Regular,
-    "MontserratBold": Montserrat_700Bold
-  });
+  const [appIsReady, setAppIsReady] = useState(false);
 
-  if (!fonteCarregada) {
-    return <AppLoading />
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync({
+          "MontserratRegular": Montserrat_400Regular,
+          "MontserratBold": Montserrat_700Bold,
+        });
+        await new Promise(resolve => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
   }
 
   return (
     <SafeAreaView style={{ fex: 1 }}>
-      <StatusBar />
-      <Cesta {...mock} />
+      <View onLayout={onLayoutRootView}>
+        <StatusBar />
+        <Cesta {...mock} />
+      </View>
     </SafeAreaView>
   );
 }
